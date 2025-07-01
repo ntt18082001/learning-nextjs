a7180ea97a0eb012d096ccaa06ea362b628c9f7e
nttien <ntt18082001@gmail.com>
2025-07-01 23:42:49 +0700

    learning router

---
+++ src/app/admin/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function DashboardPage() {
+  return (
+    <div className="flex items-center justify-center min-h-screen p-8">
+      <h1 className="text-2xl font-bold">Admin</h1>
+    </div>
+  );
+}
